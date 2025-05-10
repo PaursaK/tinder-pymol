2128f8d433ed1eda4a64a23ac9ada5baf6b20b15
pkamalian <pkamalian98@gmail.com>
2025-05-09 23:07:30 -0700

    additional comments for clarity

--- moltinder.py
+++ moltinder.py
@@ -10,13 +10,15 @@ pdb_dir = "<PATH TO CANDIDATE PDB DIRECTORY HERE>" # Directory containing candid
 colors = ["red", "green", "blue", "yellow", "cyan", "magenta", "orange", "purple", "grey", "white"]
 target_pdb = "<YOUR TARGET PATH HERE>"
 
+# Ensure the target PDB file exists
 output_dir = os.path.dirname(target_pdb)
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
+
+# Create directories for liked and disliked models
 liked_dir = os.path.join(output_dir, "liked_models")
 disliked_dir = os.path.join(output_dir, "disliked_models")
-
 os.makedirs(liked_dir, exist_ok=True)
 os.makedirs(disliked_dir, exist_ok=True)
 
@@ -27,6 +29,8 @@ print("Then, Press → or ← to like/dislike models and move onto the next mode
 print("The liked and disliked models will be saved in the same directory as the target model.")
 
 def setup_models():
+    ''' function to load the target model and candidate models into PyMOL'''
+
     # Load target model
     cmd.load(target_pdb, "target_model")
     cmd.show("cartoon", "target_model")
@@ -68,6 +72,8 @@ def next_model():
     print(f"Showing {name}.")
 
 def liked_model():
+    ''' function to save the liked model to the liked directory'''
+
     if current_index[0] >= 0 and current_index[0] < len(models):
         name = models[current_index[0]]
         output_path = os.path.join(liked_dir, f"{name}.pdb")
@@ -77,6 +83,8 @@ def liked_model():
     next_model() # Move to the next model
 
 def disliked_model():
+    ''' function to save the disliked model to the disliked directory'''
+
     if current_index[0] >= 0 and current_index[0] < len(models):
         name = models[current_index[0]]
         output_path = os.path.join(disliked_dir, f"{name}.pdb")
@@ -91,6 +99,8 @@ def disliked_model():
 from pymol import cmd
 
 def delayed_binding():
+    ''' function to bind the left and right arrow keys to the like/dislike functions'''
+
     # Wait a bit before setting the key (PyMOL UI needs to be fully ready)
     import threading
     def bind():
